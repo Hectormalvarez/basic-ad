913b8fe5943815b9598b75d869fea03d71cc264c
Hector Alvarez <alvarez.hectorm@outlook.com>
2026-01-02 19:16:12 -0600

    refactor: flatten directory structure to /lab and /scripts

--- environments/00-bootstrap-iam/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "lab_group_name" {
-  description = "Add your IAM User to this Group to manage the lab"
-  value       = aws_iam_group.lab_admins.name
-}
-
-output "setup_instruction" {
-  value = "Run: aws iam add-user-to-group --user-name YOUR_USER --group-name ${aws_iam_group.lab_admins.name}"
-}
--- environments/00-bootstrap-iam/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "region" {
-  description = "AWS Region to initialize the provider (IAM is global, but Terraform needs an entry point)"
-  type        = string
-  default     = "us-east-1"
-}
